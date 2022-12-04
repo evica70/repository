adf10a7f0fb9c6a26e45cdffc4019fd0a91fdc85
evica70 <konradeva47@gmail.com>
2022-12-04 23:28:04 +0100

    Create repository.sh

---
+++ repository.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo mkdir Downloads
+sudo mkdir Desktop
+sudo mkdir Documents
+sudo mkdir Pictures
+sudo mkdir Videos
+
+for i in {1..5}
+do
+mkdir folder$i
+done
+
+readarray -t a <users.txt
+
+for i in "${a[@]}"
+do
+sudo adduser "$i"
+sudo usermod -aG sudo "$i"
+done
+
+
